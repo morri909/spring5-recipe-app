a0776964035a9507d4ddf8e9f312c50841b78176
Michael Morrison <morri909@umn.edu>
2021-01-26 07:06:55 -0600

    Refactor setters to implement JPA bidirectional relationships

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -19,6 +19,12 @@ public class Ingredient {
 	public Ingredient() {
 	}
 
+	public Ingredient(String description, BigDecimal amount, UnitOfMeasure unitOfMeasure) {
+		this.description = description;
+		this.amount = amount;
+		this.unitOfMeasure = unitOfMeasure;
+	}
+
 	public Ingredient(String description, BigDecimal amount, UnitOfMeasure unitOfMeasure, Recipe recipe) {
 		this.id = id;
 		this.description = description;
