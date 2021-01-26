144c2b820fbe42d75e15c9e5884b86d97e7b4ccd
Michael Morrison <morri909@umn.edu>
2021-01-25 19:02:19 -0600

    Add categories many to many relationship with recipes

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -24,6 +24,13 @@ public class Recipe {
 	private Notes notes;
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
 	private Set<Ingredient> ingredients;
+	@ManyToMany
+	@JoinTable(
+			name = "recipe_category",
+			joinColumns = @JoinColumn(name = "recipe_id"),
+			inverseJoinColumns = @JoinColumn(name = "category_id")
+	)
+	private Set<Category> categories;
 
 	public void setId(Long id) {
 		this.id = id;
@@ -119,5 +126,13 @@ public class Recipe {
 
 	public void setIngredients(Set<Ingredient> ingredients) {
 		this.ingredients = ingredients;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(Set<Category> categories) {
+		this.categories = categories;
 	}
 }
