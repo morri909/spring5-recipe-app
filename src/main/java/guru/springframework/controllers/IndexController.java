77c3bc7fdf03667552b20c2bf3353af35e7b6cc1
Michael Morrison <morri909@umn.edu>
2021-01-25 19:36:58 -0600

    Implement get by description query methods

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,13 +1,32 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+	private final CategoryRepository categoryRepository;
+	private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
 	@RequestMapping({"", "/", "/index"})
 	public String getIndexPage(){
+		Optional<Category> categoryOptional = categoryRepository.findByDescription("Mexican");
+		Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+		System.out.println("Cat id is: " + categoryOptional.get().getId());
+		System.out.println("uofm id is: " + unitOfMeasureOptional.get().getId());
 		return "index";
 	}
 }
