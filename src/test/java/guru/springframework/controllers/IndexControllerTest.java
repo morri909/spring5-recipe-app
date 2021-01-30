f6c0e5bab411afe29a73a33c30bf5839c2b4ff31
Michael Morrison <morri909@umn.edu>
2021-01-29 18:54:15 -0600

    Mockito argument captor example

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -1,14 +1,15 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class IndexControllerTest {
 
@@ -27,11 +28,25 @@ public class IndexControllerTest {
 
 	@Test
 	public void getIndexPage() {
+		Set<Recipe> recipes = new HashSet<>();
+		Recipe recipe1 = new Recipe();
+		recipe1.setId(1L);
+		recipes.add(recipe1);
+		Recipe recipe2 = new Recipe();
+		recipe2.setId(2L);
+		recipes.add(recipe2);
+		Mockito.when(recipeService.getRecipes()).thenReturn(recipes);
+
+		ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
 		String view = indexController.getIndexPage(model);
 
 		Assert.assertEquals("index", view);
 		Mockito.verify(recipeService, Mockito.times(1)).getRecipes();
 		Mockito.verify(model, Mockito.times(1))
-				.addAttribute(ArgumentMatchers.eq("recipes"), ArgumentMatchers.anySet());
+				.addAttribute(ArgumentMatchers.eq("recipes"), argumentCaptor.capture());
+
+		Set<Recipe> results = argumentCaptor.getValue();
+		Assert.assertEquals(2, results.size());
 	}
 }
