9f344e8ee4af94ac648d2b9781e97b8773a819b4
Michael Morrison <morri909@umn.edu>
2021-01-29 19:14:36 -0600

    Integration test example

---
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,42 @@
+package guru.springframework.repositories;
+
+import guru.springframework.domain.UnitOfMeasure;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+
+	@Autowired
+	private UnitOfMeasureRepository sut;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void findByDescription() {
+		Optional<UnitOfMeasure> uom = sut.findByDescription("Teaspoon");
+
+		Assert.assertTrue(uom.isPresent());
+		Assert.assertEquals("Teaspoon", uom.get().getDescription());
+	}
+
+	@Test
+	public void findByDescriptionCup() {
+		Optional<UnitOfMeasure> uom = sut.findByDescription("Cup");
+
+		Assert.assertTrue(uom.isPresent());
+		Assert.assertEquals("Cup", uom.get().getDescription());
+	}
+}
