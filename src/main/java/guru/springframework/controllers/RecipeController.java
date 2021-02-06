ad079ff947bfa34743b0b3a7a5b9d104f745ab96
Michael Morrison <morri909@umn.edu>
2021-02-06 17:34:16 -0600

    Add create new recipe form and post endpoint, resolve merge conflicts

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package guru.springframework.controllers;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +23,18 @@ public class RecipeController {
 	public String showById(@PathVariable Long id, Model model) {
 		model.addAttribute("recipe", recipeService.findById(id));
 		return "recipe/show";
+	}
+
+	@RequestMapping("/new")
+	public String newRecipe(Model model) {
+		model.addAttribute("recipe", new RecipeCommand());
+		return "recipe/recipeform";
+	}
+
+	@PostMapping
+	@RequestMapping("")
+	public String save(@ModelAttribute RecipeCommand recipeCommand) {
+		RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
+		return "redirect:/recipe/show/" + savedRecipeCommand.getId();
 	}
 }
