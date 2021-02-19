b828489ed626f011a6ebbc0669d97d15dddf9a25
Michael Morrison <morri909@umn.edu>
2021-02-18 18:37:58 -0600

    Add number format exception handler

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -97,4 +97,11 @@ public class RecipeControllerTest {
 				.andExpect(status().isNotFound())
 				.andExpect(view().name("404error"));
 	}
+
+	@Test
+	public void handleBadRequest() throws Exception {
+		mockMvc.perform(get("/recipe/bad/show"))
+				.andExpect(status().isBadRequest())
+				.andExpect(view().name("400error"));
+	}
 }
